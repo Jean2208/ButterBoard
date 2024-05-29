e8a2441ae14a4b2a6c72141d1441f2fbcaf5df40
Woosung Nam <nws400@gmail.com>
2024-05-29 14:04:42 -0500

    refactor: content in two column

--- frontend/src/landing-page/LandingPage.jsx
+++ frontend/src/landing-page/LandingPage.jsx
@@ -39,6 +39,7 @@ export default function LandingPage() {
   return (
       <>
       <NavBar />
+
           <Hero />
             <OppositeContentTimeline/>
 
--- frontend/src/landing-page/components/Hero.jsx
+++ frontend/src/landing-page/components/Hero.jsx
@@ -7,79 +7,62 @@ import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid'; // Import Grid from Material-UI
+import reactLogo from '../../assets/react.svg';
+
 
 export default function Hero() {
-  return (
-    <Box
-      id="hero"
-      sx={(theme) => ({
-        width: '100%',
+    return (
+        <Box id="hero">
+            <Container>
+                <Stack spacing={1} useFlexGap sx={{ width: { xs: '100%', sm: '120%' } }}>
+                    <Grid container spacing={2}> {/* Add a Grid container */}
+                        <Grid item xs={12} sm={6}> {/* First column */}
+                            <Typography variant="h2">
+                                All Productivity&nbsp;
+                                <Typography
+                                    component="span"
+                                    variant="h2"
+                                    color={"gray"}
+                                >
+                                    Tools
+                                </Typography>
+                            </Typography>
+                            <Typography variant="h2">
+                                All In One&nbsp;
+                                <Typography
+                                    component="span"
+                                    variant="h2"
+                                    color={"gray"}
+                                >
+                                    Place
+                                </Typography>
+                            </Typography>
+                            <Typography
 
-      })}
-    >
-      <Container
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+                            >
+                                Where to grow your business as a blah blah blah  blah blah blah blah blah blah blah blah blah blah blah blah
+                                blah blah blah blah blah blah blah blah blah
+                                Where to grow your business as a blah blah blah  blah blah blah blah blah blah blah blah blah blah blah blah
+                            </Typography>
 
-        }}
-      >
-        <Stack spacing={1} useFlexGap sx={{ width: { xs: '100%', sm: '70%' } }}>
-          <Typography variant="h2">
-            All Productivity&nbsp;
-            <Typography
-              component="span"
-              variant="h2"
-              color={"gray"}
-            >
-              Tools
-            </Typography>
-          </Typography>
-            <Typography variant="h2">
-                All In One&nbsp;
-                <Typography
-                    component="span"
-                    variant="h2"
-                    color={"gray"}
-                >
-                    Place
-                </Typography>
-            </Typography>
-          <Typography
-            textAlign="center"
-            color="lightgray"
-            sx={{ alignSelf: 'center', width: { sm: '100%', md: '80%' } }}
-          >
-            Where to grow your business as a blah blah blah  blah blah blah blah blah blah blah blah blah blah blah blah
-              blah blah blah blah blah blah blah blah blah
-              Where to grow your business as a blah blah blah  blah blah blah blah blah blah blah blah blah blah blah blah
+                            <Box sx={{ marginTop: 2 }}>
+                                <Button variant="contained" color="primary">
+                                    Start now
+                                </Button>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} sm={6}> {/* Second column */}
 
+                            {/*image will go here*/}
+                            <a href="https://react.dev" target="_blank">
+                                <img src={reactLogo}  className="logo" alt="React logo"/>
+                            </a>
 
-
-          </Typography>
-          <Stack
-            direction={{ xs: 'column', sm: 'row' }}
-            alignSelf="center"
-            spacing={1}
-            useFlexGap
-            sx={{ pt: 2, width: { xs: '100%', sm: 'auto' } }}
-          >
-
-            <Button variant="contained" color="primary">
-              Start now
-            </Button>
-          </Stack>
-          <Typography variant="caption" textAlign="center" sx={{ opacity: 0.8 }}>
-            By clicking &quot;Start now&quot; you agree to our&nbsp;
-            <Link href="#" color="primary">
-              Terms & Conditions
-            </Link>
-            .
-          </Typography>
-        </Stack>
-
-      </Container>
-    </Box>
-  );
+                        </Grid>
+                    </Grid>
+                </Stack>
+            </Container>
+        </Box>
+    );
 }
